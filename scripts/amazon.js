fe5595d40a48923047974be2919c80d610d41463
Yezzel <kyeibenjamin03@gmail.com>
2024-11-21 15:08:03 +0000

    move addToCart in cart.js

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,5 +1,6 @@
-import {cart} from '../data/cart.js'
+import {cart, addToCart} from '../data/cart.js'
 import {products} from '../data/products.js'
+
 
 let putProductHtml = '';
 
@@ -63,6 +64,17 @@ products.forEach((product) => {
 
 
 
+function updateCartQuantity () {
+  let cartQuantity = 0;
+
+   cart.forEach((item) => {
+    cartQuantity = item.quantity
+   });
+   document.querySelector('.js-cart-quantity')
+   .innerHTML = cartQuantity;
+
+  
+}
 
 
 document.querySelector('.js-products-grid')
@@ -75,31 +87,8 @@ document.querySelectorAll('.add-cart').forEach((button) => {
 
    const productId = button.dataset.productId
 
-   let matchingItem;
-
-   cart.forEach((item) => {
-    if(productId === item.productId) {
-      matchingItem = item
-
-    }
-
-   });
-   if(matchingItem) {
-    matchingItem.quantity += 1
-   }else {
-    cart.push({
-      productId: productId,
-      quantity: 1
-    });
-   }
-   let cartQuantity = 0;
-
-   cart.forEach((item) => {
-    cartQuantity = item.quantity
-   });
-   document.querySelector('.js-cart-quantity')
-   .innerHTML = cartQuantity;
-
+   addToCart(productId);
+   updateCartQuantity();
   })
     })
   
