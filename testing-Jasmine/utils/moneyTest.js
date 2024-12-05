158f3cf4e4acbbe98292183b365a425b55f02e9f
Yezzel <kyeibenjamin03@gmail.com>
2024-12-05 13:35:47 +0000

    create automated tests for add To Cart

--- testing-Jasmine/utils/moneyTest.js
+++ testing-Jasmine/utils/moneyTest.js
@@ -1,4 +1,4 @@
-import { formatCurrency } from "../scripts/utils/money.js";
+import { formatCurrency } from "../../scripts/utils/money.js";
 
 describe('test suite: formatCurrency', () => {
   it('converts cents into dollars',() => {
@@ -9,7 +9,7 @@ describe('test suite: formatCurrency', () => {
     expect(formatCurrency(0)).toEqual('0.00')
   });
   it('rounds up to the nearest cent', () => {
-    expect(formatCurrency(2095)).toEqual('20.10')
+    expect(formatCurrency(2095)).toEqual('20.95')
   });
 });
 
