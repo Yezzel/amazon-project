7b38cb78efacf752acba6cf77ea913515dbf68b1
Yezzel <kyeibenjamin03@gmail.com>
2024-12-16 15:09:00 +0000

    learn promises and convert code into promises

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -3,9 +3,24 @@ import {renderPaymentSummary} from './Checkout/paymentSummary.js';
 //import '../data/cart-class.js';
 //import '../data/backend-practice.js'
 import { loadProducts } from "../data/products.js";
+Promise.all([
+  new Promise((resolve) => {
+     loadProducts(() => {
+      resolve('value1')
+    });
 
+  }).then((values) => {
+    console.log(values)
+    renderOrderSummary();
+    renderPaymentSummary();
+  })
+])
+
+
+/*
 loadProducts(() => {
   renderOrderSummary();
   renderPaymentSummary();
 });
+*/
 
